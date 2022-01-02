47657ffa4fb11cf9c40e4f4afa7e01b612cfa3bd
asvartgroupspb@gmail.com <asvartgroupspb@gmail.com>
2022-01-02 13:22:30 +0300

    Добавьте файлы проекта.

---
+++ Task_10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите данные для транспортира");
+            int grad_ = Convert.ToInt32(Console.ReadLine());
+            int minute_ = Convert.ToInt32(Console.ReadLine());
+            int second_ = Convert.ToInt32(Console.ReadLine());
+
+            Transportir transportir = new Transportir(grad_, minute_, second_);
+
+            //Transportir transportir = new Transportir() {grad=grad_, minute=minute_, second=second_};
+
+            //transportir.grad = Convert.ToInt32(Console.ReadLine());
+            //transportir.minute = Convert.ToInt32(Console.ReadLine());
+            //transportir.second = Convert.ToInt32(Console.ReadLine());
+
+            double totalRadians = transportir.ToRadians();
+            Console.WriteLine("Значение в градусах " + totalRadians);
+            Console.WriteLine("Перевод градусов в радианы " + (totalRadians / 3600 / 180 * Math.PI));
+            Console.ReadKey();
+        }
+        
+        
+
+    
+
+    }
+}
---
+++ Task_10/Transportir.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10
+{
+    class Transportir
+    {
+        int grad;
+        int minute;
+        int second;
+
+        public int Grad
+        {
+            get
+            {
+                return grad;
+            }
+
+            set
+            {
+                grad = value % 360;
+            }
+        }
+
+        public int Minute
+        {
+            get
+            {
+                return minute;
+            }
+
+            set
+            {
+                Grad += value / 60;
+                minute= value % 60;
+            }
+        }
+
+        public int Second
+        {
+            get
+            {
+                return second;
+            }
+
+            set
+            {
+                Minute += value / 60;
+                second = value % 60;
+            }
+        }
+
+
+
+        public Transportir(int grad, int minute, int second)
+        {
+            this.Grad = grad;
+            this.Minute = minute;
+            this.Second = second;
+
+        }
+
+
+
+
+        public int ToRadians()
+        {
+            return second + minute * 60 + grad * 60 * 60;
+        }
+    }
+}
